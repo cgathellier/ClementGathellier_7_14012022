0138bcf1f6c8b39c48d7d00852fba63ca5064466
cgathellier <clement.gathellier.dev@gmail.com>
2022-02-09 22:09:33 +0100

    nettoyage du readme

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Get, Param } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { User } from '@prisma/client';
 import { LoginDto, SignUpDto } from './authDto';
