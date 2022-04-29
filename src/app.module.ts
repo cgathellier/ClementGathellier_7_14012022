6aa6ef23a38ee9f836b858e63987f16f5f9e642d
cgathellier <clement.gathellier.dev@gmail.com>
2022-04-29 11:48:05 +0200

    suppression du module tasks

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './prisma/prisma.service';
 import { PostsModule } from './posts/posts.module';
@@ -7,7 +6,7 @@ import { CommentsModule } from './comments/comments.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TasksModule, AuthModule, PostsModule, CommentsModule, UsersModule],
+  imports: [AuthModule, PostsModule, CommentsModule, UsersModule],
   providers: [PrismaService],
 })
 export class AppModule {}
--- src/tasks/dto/update-task-status.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsEnum } from 'class-validator';
-import { TaskStatus } from '../tasks.model';
-
-export class UpdateTaskStatusDto {
-  @IsEnum(TaskStatus)
-  status: TaskStatus;
-}
